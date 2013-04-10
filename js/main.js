b69e96f5a8ffb4c98277026ac5cb67db590a831f
Alex Sexton <AlexSexton@gmail.com>
2013-04-10 03:48:47 -0500

    Better deeplinking

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ require({
   MBP.hideUrlBarOnLoad();
   $(function () {
     $('article h2').click(function() {
+      $('.active').not(this).removeClass('active').siblings('.details').hide();
       $(this).toggleClass('active').siblings('.details').slideToggle();
     });
+
+    var targetEl = $(window.location.hash);
+    if (targetEl.length) {
+      targetEl.find('h2').click();
+    }
   });
 });
